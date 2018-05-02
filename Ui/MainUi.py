3da6bf9659343735e37258fdfeee3483a9babbf9
fwei <fwei0630@163.com>
2018-05-02 17:23:52 +0800

    提交UI

--- Ui/MainUi.py
+++ Ui/MainUi.py
@@ -10,12 +10,14 @@ class Main_Ui(QWidget):
         self.initUI()
     
     def initUI(self):
-        self.setGeometry(300,300,400,300)
+        self.setGeometry(20,20,1280,720)
         self.setWindowTitle('公家车人群计数系统')
         
         #设置上方布局
         main_above_hbox = QHBoxLayout() #主界面上方布局
         self.video_label = self.set_label('')
+        #self.video_label.resize(960, 540)
+        self.video_label.setFixedSize(960,540)
         
         above_right_bbox = self.set_above_right_layout()
         main_above_hbox.addWidget(self.video_label)
@@ -31,12 +33,15 @@ class Main_Ui(QWidget):
         main_vbox = QVBoxLayout()
         main_vbox.addLayout(main_above_hbox)
         main_vbox.addLayout(main_blew_hbox)
+        main_vbox.setStretch(0,3)
+        main_vbox.setStretch(1,1)
 
         self.setLayout(main_vbox)
 
     def set_above_right_layout(self):
         ## 设置核心布局
-        child_right_Gridbox =  QGridLayout()
+        child_right_Gridbox = QGridLayout()
+        child_right_Gridbox.setSizeConstraint(QLayout.SetMinAndMaxSize)
         child_right_Gridbox.setColumnStretch(1, 4)
         child_right_Gridbox.setColumnStretch(2, 4)
         
@@ -45,10 +50,10 @@ class Main_Ui(QWidget):
         self.crossline_counted_label = self.set_label('0','l')
         self.crossline_sum_counted_label = self.set_label('0','l')
         
-        self.show_door_state_label = self.set_label('车门状态','r')
-        self.show_crowd_counted_label = self.set_label('车内人数','r')
-        self.show_crossline_counted_label = self.set_label('下车总人数','r')
-        self.show_crossline_sum_counted_label = self.set_label('当前下车人数','r')
+        self.show_door_state_label = self.set_label('车门状态','l')
+        self.show_crowd_counted_label = self.set_label('车内人数','l')
+        self.show_crossline_counted_label = self.set_label('下车总人数','l')
+        self.show_crossline_sum_counted_label = self.set_label('当前下车人数','l')
         
         #设置布局内容
         child_right_Gridbox.addWidget(self.show_door_state_label,0,0)
